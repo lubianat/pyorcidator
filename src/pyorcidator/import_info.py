93d0647ae20563d839e09918f1955d64196f97e7
jvfe <jvfecav@gmail.com>
2022-04-08 14:43:51 -0300

    feat: Add flag to open browser with QuickStatements
    - Closes #9

--- src/pyorcidator/import_info.py
+++ src/pyorcidator/import_info.py
@@ -1,16 +1,26 @@
-from .helper import *
 from urllib.parse import quote
+
 import click
+import requests
+
+from .helper import *
 
 
 @click.command(name="import")
 @click.option("--orcid", prompt=True, help="The ORCID Id to look up")
+@click.option(
+    "--open-browser",
+    is_flag=True,
+    help="Automatically open browser with QuickStatements",
+)
 def main(orcid: str):
     qs = render_orcid_qs(orcid)
     quoted_qs = quote(qs.replace("\t", "|").replace("\n", "||"), safe="")
     url = f"https://quickstatements.toolforge.org/#/v1={quoted_qs}\\"
     print(qs)
     print(url)
+    if open_browser:
+        webbrowser.open_new_tab(url)
 
 
 if __name__ == "__main__":
