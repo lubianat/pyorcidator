d73d8817ef4183758f742873125674be0ea6204f
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-11 12:02:32 +0200

    Update update_roles.py

--- src/pyorcidator/dictionaries/update_roles.py
+++ src/pyorcidator/dictionaries/update_roles.py
@@ -1,21 +1,16 @@
-"""Update degrees from wikidata."""
+"""Update the roles dictionary with from Wikidata."""
 
 import json
 from pathlib import Path
 
 import click
+from textwrap import dedent
 
 from pyorcidator.dictionaries import DEGREE_PATH
 from pyorcidator.wikidata_lookup import query_wikidata
 
 HERE = Path(__file__).parent.resolve()
-QUERY_ENGLISH = """\
-SELECT (?itemLabel as ?label) ?item
-WHERE {
-  ?item wdt:P279* wd:Q3529618 .
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-}
-"""
+
 QUERY = """\
 SELECT DISTINCT ?label ?item
 WHERE {
@@ -32,14 +27,22 @@ def _removeprefix(s: str, prefix: str) -> str:
 
 
 @click.command(name="update-degrees")
-def main():
+@click.option("--parent", default="Q3529618")
+def main(parent):
     """Update the degrees lookup able from Wikidata."""
+    query = dedent(f"""\
+        SELECT (?itemLabel as ?label) ?item
+        WHERE {{
+          ?item wdt:P279* wd:{parent} .
+          SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
+        }}
+    """)
     data = json.loads(DEGREE_PATH.read_text())
     data.update(
         (record["label"]["value"], _removeprefix(record["item"]["value"], "http://www.wikidata.org/entity/"))
-        for record in query_wikidata(QUERY_ENGLISH)
+        for record in query_wikidata(query)
     )
-    DEGREE_PATH.write_text(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
+    DEGREE_PATH.write_text(json.dumps(data, indent=2, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
